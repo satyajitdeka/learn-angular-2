3f1bcc173a16db91cc61f43625c7023350a65331
Satyajit Deka <sdeka@zaloni.com>
2016-03-23 14:55:16 +0530

    angular-2-with-gulp

---
+++ angular-2-with-gulp/dev/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>Hello World!</h1>
+        <h2>Angular 2 environment setup done !</h2>
+    `,
+})
+export class AppComponent {
+
+}
