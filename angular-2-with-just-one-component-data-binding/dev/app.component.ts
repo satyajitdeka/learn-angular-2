31d23ac6145c9bf0638f493525b0b2eb74c2a3ba
Satyajit Deka <sdeka@zaloni.com>
2016-03-23 16:56:21 +0530

    angular-2-with-just-one-component-data-binding

---
+++ angular-2-with-just-one-component-data-binding/dev/app.component.ts
@@ -0,0 +1,24 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h3>{{myContact.name}}</h3>
+    <input [(ngModel)]="myContact.name" type="text">
+    <h3 (click)="showAll()" [class.clicked]="showDetails">Click here</h3>
+    <div *ngIf="showDetails">
+    {{myContact.addres}} <br>
+    {{myContact.phone}}
+    </div>
+    `,
+    styleUrls: ["../src/css/app.css"]
+})
+export class AppComponent {
+    public myContact = {name: "Satyajit Deka", addres: "Guwahati", phone: "123456789"};
+    
+    public showDetails = false;
+    
+    showAll() {
+        this.showDetails = true;
+    }
+}
