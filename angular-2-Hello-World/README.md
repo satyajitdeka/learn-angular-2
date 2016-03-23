1925f31ee9abf5452b7d59716205a1427a72d6a9
Satyajit Deka <sdeka@zaloni.com>
2016-03-23 14:46:14 +0530

    delete

--- angular-2-Hello-World/README.md
+++
@@ -1,6 +0,0 @@
-1. Install nodeJS
-2. Install typeScript
-3. Type command "npm install"
-4. Type command "npm start"
-
-And see @ http://localhost:3000/ 
--- angular-2-Hello-World/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'my-app',
-    template: '<h1>Angular 2 App</h1><h2>Hello World</h2>'
-})
-export class AppComponent { }
